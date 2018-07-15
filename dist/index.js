3c1a2be648794b6631fb7ab9f7f8bb17424b31c3
marinda <hello@marinda.me>
2018-07-15 21:04:05 +0100

    Chore: Fixing vulnerabilities via npm audit fix

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,11 @@
 'use strict';
 
-var _colorNames = require('./color-names.json');
-
-var _colorNames2 = _interopRequireDefault(_colorNames);
-
-var _uniqueRandomArray = require('unique-random-array');
-
-var _uniqueRandomArray2 = _interopRequireDefault(_uniqueRandomArray);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var getRandomItem = (0, _uniqueRandomArray2.default)(_colorNames2.default);
+var colorNames = require('./color-names.json'),
+    uniqueRandomArray = require('unique-random-array'),
+    getRandomItem = uniqueRandomArray(colorNames);
 
 module.exports = {
-  allColors: _colorNames2.default,
+  allColors: colorNames,
   randomColor: function randomColor(number) {
     if (number === 'undefined' || typeof number !== 'number') {
       return getRandomItem();
